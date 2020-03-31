6fbaa40119fef52a3c1fca41948e37fac23f1f94
ShinkarenkoAlexey <alex.shinkarenko17@gmail.com>
2020-03-31 14:01:27 +0300

    fixed 6 in 4lab

--- Labs/lab4/6.py
+++ Labs/lab4/6.py
@@ -1,14 +1,17 @@
 s = int(input("Номер символа..."))
 h = int(input("Высота..."))
 s = chr(s)
-s2 = "||"
+s1 = " "
+d = h
 x = 0
 for j in range(0, h+1):
     if j != h:
         while j > 0:
             j -= 1
         if x != h:
-            print('{:>25}||{}'.format(s*x, s*x), end='')
+            print('{}{}||{}{}'.format(s1*d, s*x, s*x, s1*d), end='')
             print(end='\n')
             x += 1
-    else: print('{:25}||{}'.format(s*0, s*0), end='')
+            d -= 1
+    else: print('{}{}||{}{}'.format(s1*h, s*0, s*0, s1*h), end='')
+
