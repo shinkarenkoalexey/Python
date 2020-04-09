8d5663abd97ed5b276b6aacb440373dbc474d0a6
ShinkarenkoAlexey <alex.shinkarenko17@gmail.com>
2020-04-09 20:02:39 +0300

    without if

--- Labs/lab4/4.py
+++ Labs/lab4/4.py
@@ -1,14 +1,14 @@
 A = int(input("A..."))
 B = int(input("B..."))
 
-s = [i for i in range(A, B+1)]
+s = [i for i in range(B, A+1)]
 x = 1
 while s[-1] % 2 == 1 and x > 0:
     for i in s[::-2]:
         print(i, end=" ")
     x = 0
 while s[-1] % 2 == 0 and x > 0:
-    s.remove(B)
+    s.remove(A)
     for i in s[::-2]:
         print(i, end=" ")
     x = 0
