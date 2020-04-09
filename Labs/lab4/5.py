56ea0e45b66d64064ab5e6b3220cafffb3edf195
ShinkarenkoAlexey <alex.shinkarenko17@gmail.com>
2020-04-09 21:16:17 +0300

    add dividers

--- Labs/lab4/5.py
+++ Labs/lab4/5.py
@@ -1,6 +1,7 @@
 A = int(input("A..."))
 B = int(input("B..."))
 s = []
+s1 = []
 k = 0
 x = 0
 for i in range(A, B+1):
@@ -8,14 +9,18 @@ for i in range(A, B+1):
     while x != 0:
         if i % x == 0:
             k += 1
+            s1.append(x)
             x -= 1
         else:
             x -= 1
     if k >= 3:
         s.append(i)
+    else:
+        s1.clear()
     if s:
-        print(s, "Количество делителей -", k)
+        print(s, "Количество делителей -", k, s1)
         k = 0
         s.clear()
+        s1.clear()
     else:
         k = 0
